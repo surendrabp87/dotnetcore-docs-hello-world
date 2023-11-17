be7559bbac88685c6e65ddd6abc45fde4b6d90da
surendrabp87 <83505190+surendrabp87@users.noreply.github.com>
2023-11-17 07:49:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
